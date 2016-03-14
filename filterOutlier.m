d5558ece87d7bfc976f95b79113bc0d6131b14d5
yuh.huang <hyh0221@yuhhuangs-MacBook-Pro.local>
2016-03-14 14:44:11 -0400

    fix

--- filterOutlier.m
+++ filterOutlier.m
@@ -1,6 +1,7 @@
 function [cn1, cn2] = filterOutlier(corner1, corner2, transform, threshold)
 %filter out unmatch corner correspondence in corner set
 index = 1;
+
 for i = 1 : size(corner1, 1)
    response = transform * [corner1(i, :), 1]';
    response = response / response(3); % make the response to [x, y, 1]
